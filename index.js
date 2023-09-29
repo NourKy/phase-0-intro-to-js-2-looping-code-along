e603b2394b0cc56474ed9c12b41c39837fbffc0f
Nour kayyali <nourkayyali91@gmail.com>
2023-09-30 00:28:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+const names=["Guadalupe", "Ollie", "Aki"];
+writeCards(names,"surprise");
+function writeCards(names, eventName)
+{
+    const msg=[];
+for (let i=0;i<names.length;i++)
+{
+    //console.log(`Thank you, ${names[i]} for the wonderful ${eventName} gift!`)
+    msg.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+
+}
+return msg;
+
+}
+countDown(10);
+function countDown(num)
+{
+while (num!=-1)
+{
+    console.log(num);
+    num--;
+}
+}
